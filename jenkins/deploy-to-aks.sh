5b68bb5af77b8011b41ea9f26d974cb2e8c2c8f5
cljung <christer.ljung@ikea.com>
2018-11-08 20:54:15 +0100

    syntax

--- jenkins/deploy-to-aks.sh
+++ jenkins/deploy-to-aks.sh
@@ -6,7 +6,8 @@ CONTAINERVERSION=$2
 
 echo "Deploying : ResourceGroup=$AZRGNAME, ACR=$AZACRNAME, AKS=$AZAKSNAME, Location=$AZLOCATION"
 
-az acr login --name $ACRNAME
+# login to ACR/AKS
+az acr login --name $AZACRNAME
 az aks get-credentials --resource-group $AZRGNAME --name $AZAKSNAME
 
 # get the server name of the ACR
@@ -15,19 +16,27 @@ ACRLOGINSERVER=$(az acr show --resource-group $AZRGNAME --name $AZACRNAME --quer
 # tag the container for ACR and push it
 docker tag $CONTAINERNAME:$CONTAINERVERSION $ACRLOGINSERVER/$CONTAINERNAME:$CONTAINERVERSION
 docker tag $CONTAINERNAME:$CONTAINERVERSION $ACRLOGINSERVER/$CONTAINERNAME:latest
-docker push $AZACRLOGINSERVER/$CONTAINERNAME:latest
+docker push $ACRLOGINSERVER/$CONTAINERNAME:latest
 
 # modify the deployment yaml
-sed -i -e "s/xxx-CONTAINERNAME-xxx/$CONTAINERNAME/g" ./build/deploy-aks-generic.yaml
-sed -i -e "s/xxx-replace-me-ACRLOGINSERVER-xxx/$AZACRLOGINSERVER/g" ./build/deploy-aks-generic.yaml
+cp ./deploy-aks-generic.yaml ./deploy-aks.yaml
+sed -i -e "s/xxx-CONTAINERNAME-xxx/$CONTAINERNAME/g" ./deploy-aks.yaml
+sed -i -e "s/xxx-replace-me-ACRLOGINSERVER-xxx/$ACRLOGINSERVER/g" ./deploy-aks.yaml
 
 # deploy
-kubectl apply -f ./build/deploy-aks-generic.yaml
+kubectl apply -f ./deploy-aks.yaml
+
+rm ./deploy-aks.yaml
 
 # get the http dns name for the AKS cluster and modify the ingest yaml file
-DNSZONE=$(az aks show --resource-group $AZRGNAME --name $AZAKSCLUSTERNAME --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table | tail -n1)
-sed -i -e "s/xxx-CONTAINERNAME-xxx/$CONTAINERNAME/g" ./build/ingress-aks-generic.yaml
-sed -i -e "s/xxx-replace-me-DNSZONE-xxx/$DNSZONE/g" ./build/ingress-aks-generic.yaml
+cp ./ingress-aks-generic.yaml ./ingress-aks.yaml
+DNSZONE=$(az aks show --resource-group $AZRGNAME --name $AZAKSNAME --query "addonProfiles.httpapplicationrouting.config.httpapplicationroutingzonename" -o table | tail -n1)
+sed -i -e "s/xxx-CONTAINERNAME-xxx/$CONTAINERNAME/g" ./ingress-aks.yaml
+sed -i -e "s/xxx-replace-me-DNSZONE-xxx/$DNSZONE/g" ./ingress-aks.yaml
 
 # deploy
-kubectl apply -f ./build/ingress-aks-generic.yaml
+kubectl apply -f ./ingress-aks.yaml
+
+rm ./ingress-aks.yaml
+
+# az aks browse --resource-group $AZRGNAME --name $AZAKSNAME
