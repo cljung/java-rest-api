049496d5c69a85ee4496cdbd8ea2cff4224a946e
Christer Ljung <cljung@users.noreply.github.com>
2018-11-13 12:28:07 +0100

    Update deploy-to-aks.sh

--- jenkins/deploy-to-aks.sh
+++ jenkins/deploy-to-aks.sh
@@ -12,8 +12,10 @@ az aks get-credentials --resource-group $AZRGNAME --name $AZAKSNAME
 
 # get the server name of the ACR
 ACRLOGINSERVER=$(az acr show --resource-group $AZRGNAME --name $AZACRNAME --query "loginServer" --output tsv)
+echo "ACRLOGINSERVER: $ACRLOGINSERVER"
 
 # tag the container for ACR and push it
+echo "docker tag/push"
 docker tag $CONTAINERNAME:$CONTAINERVERSION $ACRLOGINSERVER/$CONTAINERNAME:$CONTAINERVERSION
 docker tag $CONTAINERNAME:$CONTAINERVERSION $ACRLOGINSERVER/$CONTAINERNAME:latest
 docker push $ACRLOGINSERVER/$CONTAINERNAME:latest
@@ -21,6 +23,7 @@ docker push $ACRLOGINSERVER/$CONTAINERNAME:latest
 #DNSZONE=$(az aks show --resource-group $AZRGNAME --name $AZAKSNAME --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table | tail -n1)
 # when we set our own custome http application proxy name, AKS creates a new attribute all in lower case
 DNSZONE=$(az aks show --resource-group $AZRGNAME --name $AZAKSNAME --query "addonProfiles.httpapplicationrouting.config.httpapplicationroutingzonename" -o tsv)
+echo "DNSZONE: $DNSZONE"
 
 # modify the deployment yaml
 cp ./jenkins/deploy-aks-generic.yaml ./jenkins/deploy-aks.yaml
@@ -29,6 +32,7 @@ sed -i -e "s/xxx-replace-me-ACRLOGINSERVER-xxx/$ACRLOGINSERVER/g" ./jenkins/depl
 sed -i -e "s/xxx-replace-me-DNSZONE-xxx/$DNSZONE/g" ./jenkins/deploy-aks.yaml
 
 # deploy
+echo "kubectl apply"
 kubectl apply -f ./jenkins/deploy-aks.yaml
 
 rm ./jenkins/deploy-aks.yaml
